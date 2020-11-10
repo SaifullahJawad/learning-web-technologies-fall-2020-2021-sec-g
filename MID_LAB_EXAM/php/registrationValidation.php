d646834e8c906e35e9a3facd52c8469a36c429c8
SaifullahJawad <18-36818-1@student.aiub.edu>
2020-11-10 09:30:54 +0600

    first commit

---
+++ MID_LAB_EXAM/php/registrationValidation.php
@@ -0,0 +1,162 @@
+<?php
+
+
+    if(!isset($_POST["submit"]))
+    {
+
+        header("HTTP/1.0 404 Not Found");
+        exit(); 
+
+    }
+
+    if(!isset($_SESSION))
+    {
+        session_start();
+    }
+
+    $errors = ["id" => "", "password" => "", "confirmPassword" => "", "name" => "", "userType" => ""];
+    $previousInput = ["id" => "", "name" => "", "userType" => "", "password" => ""];
+    
+    
+
+
+
+    if(empty($_POST["name"]))
+    {
+        $errors["name"]= "*required";
+    }
+    else
+    {
+        $previousInput["name"] = $_POST["name"];
+        if(!isValidName($_POST["name"]))
+        {
+            $errors["name"]= "*invalid";
+        }
+    }
+
+
+
+
+
+    if(empty($_POST["id"]))
+    {
+        $errors["id"] = "*required";
+    }
+    else
+    {
+        $previousInput["id"] = $_POST["id"];
+    }
+
+
+
+    if(empty($_POST["password"]))
+    {
+        $errors["password"]= "*required";
+    }
+    else if(empty($_POST["confirmPassword"]))
+    {
+        $previousInput["password"] = $_POST["password"];
+        $errors["confirmPassword"]= "*required";
+    }
+    else
+    {
+        
+        if(!isValidPassword($_POST["password"], $_POST["confirmPassword"]))
+        {
+            $errors["password"]= "*password mismatch";
+        }
+        else
+        {
+            $previousInput["password"] = $_POST["password"];
+            $previousInput["confirmPassword"] = $_POST["confirmPassword"];
+        }
+        
+    }
+
+
+
+    if( !isset($_POST["userType"]) )
+    {
+        $errors["userType"] = "*required";
+    }
+    else
+    {
+        $previousInput["userType"] = $_POST["userType"];
+    }
+
+
+   
+
+    
+
+    if( $errors["id"] == "" && $errors["password"] == "" && $errors["confirmPassword"] =="" && $errors["name"] =="" && $errors["userType"] =="" )
+    {
+        
+
+        $myFile = fopen("userData.txt", "a");
+        
+        
+        
+
+        unset($_SESSION["previousInput"]);
+        unset($_SESSION["errors"]);
+
+
+        header("Location: logIn.php");
+    }
+    else
+    {
+        $_SESSION["errors"] = $errors;
+        $_SESSION["previousInput"] = $previousInput;
+    }
+
+
+
+
+
+    function isValidName( $name )
+    {
+        if(str_word_count($name) >= 2  && ctype_alpha($name[0]) &&  ctype_alpha(str_replace(" ", "", $name)) )
+        { 
+            return true;
+        }
+        else if(str_word_count(str_replace(".", " ",$name)) >= 2 && ctype_alpha($name[0]) &&  ctype_alpha(str_replace(".", "", $name)) && $name[strlen($name)-1] != "." )
+        {
+            return true;
+        }
+        else if(str_word_count(str_replace("-", " ",$name)) >= 2 && ctype_alpha($name[0]) &&  ctype_alpha(str_replace("-", "", $name)) && $name[strlen($name)-1] != "-" )
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+
+
+    function isValidPassword( $password, $confirmPassword )
+    {
+        if($password == $confirmPassword)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+
+
+
+
+
+
+
+    
+
+?>
+
+
---
+++ MID_LAB_EXAM/view/registration.php
@@ -0,0 +1,71 @@
+<?php
+
+	if(!isset($_SESSION))
+	{
+		session_start();
+	}
+
+	// if(isset($_SESSION["isLoggedIn"]) )
+	// {
+	// 	header("Location: dashboard.php");
+	// }
+
+	if(isset($_POST["submit"]))
+	{
+		require_once("../php/registrationValidation.php");
+	}
+	else
+	{
+		
+		unset($_SESSION["previousInput"]);
+		unset($_SESSION["errors"]);
+	
+	}
+
+
+
+
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+<center>
+<form>
+	<table border="0" cellspacing="0" cellpadding="0">
+		<tr>
+			<td>
+				<fieldset>
+					<legend><h3>REGISTRATION</h3></legend>
+					Id<br/><input type="text" name="id" value= "<?php echo $_SESSION["previousInput"]["id"] ?? ""; ?>"> <?php echo $_SESSION["errors"]["id"] ?? "" ; ?> <br/>
+
+					Password<br/><input type="password" name="password"> <?php echo $_SESSION["errors"]["password"] ?? "" ; ?> <br/>
+
+					Confirm Password<br/><input type="password" name="confirmPassword"> <?php echo $_SESSION["errors"]["confirmPassword"] ?? "" ; ?> <br/>
+
+
+					Name<br/><input type="text" name="name" value= "<?php echo $_SESSION["previousInput"]["name"] ?? ""; ?>" > <?php echo $_SESSION["errors"]["name"] ?? "" ; ?> <br/>
+
+
+					User Type<hr/>
+					<?php $userOption = $_SESSION["previousInput"]["userType"] ?? ""; ?>
+					<input type="radio" name="userType" value="User" <?php if($userOption == "User") { echo "checked" ;} ?> />User 
+					<input type="radio" name="userType" value="Admin" <?php if($userOption == "Admin") { echo "checked" ;} ?> />Admin <?php echo $_SESSION["errors"]["userType"] ?? "" ; ?>
+					<hr/>
+					<input type="submit" name="submit" value="Sign Up">
+					<a href="login.html">Sign In</a>
+				</fieldset>
+			</td>
+		</tr>                                
+	</table>
+</form>
+</center>
+		
